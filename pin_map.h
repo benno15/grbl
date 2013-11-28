de64f13c150beb5bb5d41db57b5d55b9653b2250
benno15 <mr.benjaminwood@gmail.com>
2013-11-27 20:52:19 -0800

    Update pin_map.h
    Changed

--- pin_map.h
+++ pin_map.h
@@ -34,11 +34,11 @@
   // NOTE: All step bit and direction pins must be on the same port.
   #define STEPPING_DDR       DDRD
   #define STEPPING_PORT      PORTD
-  #define X_STEP_BIT         2  // Uno Digital Pin 2
-  #define Y_STEP_BIT         3  // Uno Digital Pin 3
+  #define X_STEP_BIT         11  // Uno Digital Pin 2
+  #define Y_STEP_BIT         6  // Uno Digital Pin 3
   #define Z_STEP_BIT         4  // Uno Digital Pin 4
-  #define X_DIRECTION_BIT    5  // Uno Digital Pin 5
-  #define Y_DIRECTION_BIT    6  // Uno Digital Pin 6
+  #define X_DIRECTION_BIT    3  // Uno Digital Pin 5
+  #define Y_DIRECTION_BIT    5  // Uno Digital Pin 6
   #define Z_DIRECTION_BIT    7  // Uno Digital Pin 7
   #define STEP_MASK ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
   #define DIRECTION_MASK ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
